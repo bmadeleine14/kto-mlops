49b3ed4fccbaa806af0a685a8945faba52074a8b
bmadeleine14 <162314073+bmadeleine14@users.noreply.github.com>
2025-02-03 14:07:23 +0000

    Script corrigé

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -3,21 +3,21 @@ import unittest
 """
 Count names with more than seven letters
 """
-def names(prenoms):
-    more_than_seven = 0
+def compter_prenoms(prenoms):
+    prenom_plus_de_sept = 0
     for prenom in prenoms:
         if len(prenom) > 7:
-            more_than_seven += 1
+            prenom_plus_de_sept += 1
             print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
         else:
             print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+    return prenom_plus_de_sept
 
 class TestNamesMethod(unittest.TestCase):
      def test_names(self):
         prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
-        self.assertEqual(more_than_seven, 4)
+        prenom_plus_de_sept = compter_prenoms(prenoms=prenoms)
+        self.assertEqual(prenom_plus_de_sept, 4)
 
 if __name__ == '__main__':
     unittest.main()
